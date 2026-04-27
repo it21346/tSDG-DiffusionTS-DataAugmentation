d4580b5538dab98ac3eca3bbdcf6583e96b61d9a
it21346 <steliosbombai@gmail.com>
2026-04-27 15:25:51 +0300

    Refinement of repo: removed unused files, and added/updated any remaining.

--- Diffusion-TS/Utils/metric_utils.py
+++ Diffusion-TS/Utils/metric_utils.py
@@ -70,7 +70,7 @@ def extract_time (data):
   return time, max_seq_len
 
 
-def visualization(ori_data, generated_data, analysis, compare=3000):
+def visualization(ori_data, generated_data, analysis, dataset_name = None, compare=3000):
     """Using PCA or tSNE for generated and original data visualization.
   
   Args:
@@ -122,7 +122,7 @@ def visualization(ori_data, generated_data, analysis, compare=3000):
         plt.title('PCA plot')
         plt.xlabel('x-pca')
         plt.ylabel('y_pca')
-        plt.savefig("ETTh1_pca_plot.png", dpi=600)
+        plt.savefig(f"{dataset_name}_pca_plot.png", dpi=600)
         plt.show()
 
     elif analysis == 'tsne':
@@ -147,7 +147,7 @@ def visualization(ori_data, generated_data, analysis, compare=3000):
         plt.title('t-SNE plot')
         plt.xlabel('x-tsne')
         plt.ylabel('y_tsne')
-        plt.savefig("ETTh1_tsne_plot.png", dpi=600)
+        plt.savefig(f"{dataset_name}_tsne_plot.png", dpi=600)
         plt.show()
 
     elif analysis == 'kernel':
@@ -168,7 +168,7 @@ def visualization(ori_data, generated_data, analysis, compare=3000):
 
         # plt.savefig(str(args.save_dir)+"/"+args.model1+"_histo.png", dpi=100,bbox_inches='tight')
         # plt.ylim((0, 12))
-        plt.savefig("ETTh1_kernel_density_plot.png", dpi=600)
+        plt.savefig(f"{dataset_name}_kernel_density_plot.png", dpi=600)
         plt.show()
         
         plt.close()
